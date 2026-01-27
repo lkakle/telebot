242d04f891c64e5a02cb654ed3e5029cfc9820ec
matvei <msmsma@gmail.com>
2026-01-28 00:41:26 +0300

    обновление инструкций

--- handlers/routes.py
+++ handlers/routes.py
@@ -6,11 +6,11 @@ router = Router()
 
 @router.message(Command("start"))
 async def start(message: Message):
-    await message.answer("Команды:\n/all_users_ids\n/help")
+    await message.answer("Команды:\n/all_users_ids\n/help\nПогода ваш город")
 
 @router.message(Command("help"))
 async def start(message: Message):
-    await message.answer("Команды:\n/all_users_ids\n/help")
+    await message.answer("Команды:\n/all_users_ids\n/help\nПогода ваш город\nРазработка: @qikss")
 
 
 
